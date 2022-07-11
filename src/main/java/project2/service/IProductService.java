91abfc97d9deef01eb0ec03ca6ceda5f0370ccea
thienhaudn1993 <87653095+thienhaudn1993@users.noreply.github.com>
2022-07-11 11:34:06 +0700

    HauLST

--- src/main/java/project2/service/IProductService.java
+++ src/main/java/project2/service/IProductService.java
@@ -1,4 +1,18 @@
 package project2.service;
 
+import project2.model.Product;
+
+import java.util.List;
+
 public interface IProductService {
+    //HauLST
+    List<Product> getAllProductAuntion();
+    //HauLST
+    List<Product> getAllProductFinishedAuntion();
+    //HauLST
+    List<Product> gettAllProductAuntionAndTypeProduct(String nameTypeProduct);
+    //HauLST
+    List<Product> searchProductByNameByTypeProductByPrice(String nameProduct, String nameTypeProduct, Double min, Double max);
+    //HauLST
+    List<Product> searchProductPricesOver250(String nameProduct, String nameTypeProduct, Double min);
 }
