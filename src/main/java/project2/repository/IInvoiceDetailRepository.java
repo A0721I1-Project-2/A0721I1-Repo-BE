5b239263a86587b0502941a02397853e5b68464f
unknown <quochuy1999vn@gmail.com>
2022-07-25 20:33:46 +0700

    fix

--- src/main/java/project2/repository/IInvoiceDetailRepository.java
+++ src/main/java/project2/repository/IInvoiceDetailRepository.java
@@ -48,7 +48,7 @@ public interface IInvoiceDetailRepository extends JpaRepository<InvoiceDetail, L
                     "    on invoice.id_payment = payment.id_payment\n" +
                     "    inner join `member`\n" +
                     "    on invoice.id_member = `member`.id_member " +
-                    "where invoice.flag_delete = 0;", nativeQuery = true)
+                    "where invoice.flag_delete = 0 ", nativeQuery = true)
     Page<InvoiceDetail> findAllTransaction(Pageable pageable);
 
     // Huy-NCQ Search Seller, Buyer, NameProduct và Status
@@ -79,7 +79,7 @@ public interface IInvoiceDetailRepository extends JpaRepository<InvoiceDetail, L
             "            and name_member like concat('%', ?1, '%')" +
             "            and full_name_receiver like concat('%', ?2, '%')" +
             "            and name_product like concat('%', ?3, '%') " +
-                    "    and invoice.status_invoice = ?4", nativeQuery = true)
+                    "    and invoice.status_invoice = ?4 ", nativeQuery = true)
     Page<InvoiceDetail> searchTransaction(String nameSeller, String nameBuyer, String nameProduct, String status, Pageable pageable);
 
     /* Huy-NCQ search date */
@@ -93,7 +93,7 @@ public interface IInvoiceDetailRepository extends JpaRepository<InvoiceDetail, L
             "            inner join `member` " +
             "            on invoice.id_member = `member`.id_member " +
             "            where invoice.flag_delete = 0 " +
-            "            and product.end_date between ?1 and ?2 " ,
+            "            and DATE(product.end_date) between ?1 and ?2 " ,
             countQuery = "select invoice.id_invoice, end_date, invoicedetail.id_invoice_detail, invoice.flag_delete, payment.full_name_receiver, invoice.id_payment, invoice.id_member, invoicedetail.id_product, invoice.status_invoice, `member`.name_member,final_price, initial_price, payment.fee_service from product\n" +
                     "            inner join invoicedetail " +
                     "            on invoicedetail.id_product = product.id_product " +
@@ -104,7 +104,7 @@ public interface IInvoiceDetailRepository extends JpaRepository<InvoiceDetail, L
                     "            inner join `member` " +
                     "            on invoice.id_member = `member`.id_member " +
                     "            where invoice.flag_delete = 0 " +
-                    "            and product.end_date between ?1 and ?2 ", nativeQuery = true)
+                    "            and DATE(product.end_date) between ?1 and ?2 ", nativeQuery = true)
     Page<InvoiceDetail> searchDate(String startDate, String endDate, Pageable pageable);
 
     // Huy-NCQ ẩn record flag_delete
